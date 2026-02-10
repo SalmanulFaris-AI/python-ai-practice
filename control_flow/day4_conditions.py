f89ad29b7bb23a342aecff8c7983f7d8e361157d
SalmanulFaris-AI <sammufari123@gmail.com>
2026-02-10 18:12:09 +0530

    Added day4 condition practice

---
+++ control_flow/day4_conditions.py
@@ -0,0 +1,12 @@
+age=18
+if age >=18:
+  print("you are eligible to vote")
+else:
+  print("you are note eligible to vote")
+
+
+number=7
+if number %2==0:
+  print("Even number")
+else:
+  print("Odd number")
